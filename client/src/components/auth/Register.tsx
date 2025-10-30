d03bfc1419969a4b8a0da9a68642657873e85497
thanhhuyen13 <buithithanhhuyen0404@gmail.com>
2025-10-30 14:05:42 +0700

    Save local changes before merge

--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -92,7 +92,7 @@ const Register = () => {
             console.log('Sending registration request to:', `${API_BASE_URL}/auth/register`);
             console.log('With data:', formData);
             console.log('Attempting to register with:', formData);
-            const response = await fetch('http://localhost:5000/api/auth/register', {
+            const response = await fetch(`${API_BASE_URL}/auth/register`, {
                 method: 'POST',
                 headers: { 
                     'Content-Type': 'application/json'
