597de0c0c67e339f829ada2f243a78f0e6eda101
diendan2427 <thanhnguyen848878@gmail.com>
2025-12-05 14:01:40 +0700

    FixProblemDetail

--- client/src/components/practice/ProblemDetail.tsx
+++ client/src/components/practice/ProblemDetail.tsx
@@ -517,7 +517,7 @@ export function ProblemDetail({ problemId, onSubmissionSuccess }: ProblemDetailP
             </>
           )}
 
-          {activeTab === "editorial" && (
+          {activeTab === "editorial" && problemId &&(
             <CommentSection challengeId={problemId} />
           )}
 
